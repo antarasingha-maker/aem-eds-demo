f4c81b22f07be68eac8eb3ca096b41f426a48a38
Antara Singha <conasin1@partner.waters.com>
2026-06-11 15:52:05 +0530

    commit fourteen debugging

---
+++ blocks/article-feature/article-feature.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+  block.innerHTML = `
+    <div style="background:red;padding:40px;color:white;font-size:40px">
+      ARTICLE FEATURE WORKING
+    </div>
+  `;
+}
